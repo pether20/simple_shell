72e8dad5097b81c10e2cf584da0b080e0bc25e2d
pether20 <pether.te.z@hotmail.com>
2021-12-08 23:36:13 -0500

    Delete line 5

--- generate-authors.sh
+++ generate-authors.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/.."
 
 {
 	cat <<- 'EOH'
-		# This file lists all individuals having contributed content to the repository.
+		# This file lists all individuals having contributed.
 		# For how it is generated, see `hack/generate-authors.sh`.
 	EOH
 	echo
