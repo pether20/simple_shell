67e9f806099a883ac43aecadce3655c0ee616c3f
pether20 <pether.te.z@hotmail.com>
2021-12-08 22:54:11 -0500

    delete function iscommand in line 41

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
  * @argv: arguments
  * @env: enviroments
  * Return: success full 1 or fail -1
- */
+ **/
 int main(int argc, char **argv, char **env)
 {
-	char delimitador[] = ",.;\n\t ";
-	var_t vars = {0, 0, -1, NULL, NULL, NULL};
+	char *prompt = "$ ", delimitador[] = ",.;\n\t ";
+	var_t vars = {0, 0, 0, NULL, NULL, NULL};
 	size_t size = 0;
 	int i = 0;
 
@@ -28,22 +28,23 @@ int main(int argc, char **argv, char **env)
 
 	while (getline(&vars.comand, &size, stdin) != -1)
 	{
-		vars.tk_i++;
 		i = 0;
 		vars.tokens[i] = strtok(vars.comand, delimitador);
 		while (vars.tokens[i]  != NULL)
 		{
+			vars.tk_i = i;
 			i++;
+
 			vars.tokens[i] = strtok(NULL, delimitador);
 			vars.contk = i;
 		}
 
-		isCommand(&vars, env);
-		if (isatty(STDIN_FILENO))
-			write(1, "$ ", 3);
+		execute(&vars, env);
+		printf("%s", prompt);
 		free(vars.comand);
 		vars.comand = NULL;
 		size = 0;
+
 	}
 	free(vars.comand);
 	free(vars.tokens);
